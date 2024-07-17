57d164a7a2e3cac59fad3e2a45bcaee9a5f78bfd
APIVerve Team <hello@apiverve.com>
2024-07-17 12:19:20 -0500

    Bug Fixes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class moonphasesWrapper {
             query = {};
         }
 
-        var requiredParams = ["date"];
+        var requiredParams = [];
         if (requiredParams.length > 0) {
             for (var i = 0; i < requiredParams.length; i++) {
                 if (!query[requiredParams[i]]) {
